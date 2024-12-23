b5608d908763bea5736760b8ca11b2204f62f000
rekhasahoo <132289850+rekhasahoo@users.noreply.github.com>
2024-12-23 12:45:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,6 +96,6 @@ def preprocess_email_transcription(transcription):
 '''if __name__ == '__main__':
     app.run(debug=True)'''
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
 
